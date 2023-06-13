98d2936108f644239457d6f20fd360da02afb877
sangki930 <sangki931@gmail.com>
2023-06-14 00:21:59 +0900

    commit 카드바꾸기

--- src/implementation/카드바꾸기/Main.java
+++ src/implementation/카드바꾸기/Main.java
@@ -3,9 +3,7 @@ package implementation.카드바꾸기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,27 +11,25 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         double[] arr = Arrays.stream(br.readLine().split(" ")).mapToDouble(i->Double.parseDouble(i)).toArray();
-        Set<Integer> set = new HashSet<>();
+        Map<String,HashSet<String>> map = new HashMap<>();
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 double d = (arr[j]-arr[i])/(j-i);
                 if((int)Math.floor(d)==(int)d){
-                   set.add((int)d);
+                   String formula = "y="+d+"x+"+(-d*i+arr[i]);
+                   HashSet<String> set = map.getOrDefault(formula,new HashSet<>());
+                   set.add(Arrays.toString(new double[]{i,arr[i]}));
+                   set.add(Arrays.toString(new double[]{j,arr[j]}));
+                   map.put(formula,set);
                 }
             }
         }
-        System.out.println(set);
-        int answer = Integer.MAX_VALUE;
-        for(int diff : set){
-            int cnt = 0;
-            for(int i=0;i<arr.length;i++){
-                if(diff*i*1.0+arr[0]==arr[i]){
-                    cnt++;
-                }
-            }
-            answer = Math.min(answer,cnt);
+
+        int cnt = 0;
+        for(String key : map.keySet()){
+            cnt = Math.max(cnt,map.get(key).size());
         }
-        System.out.println(answer);
+        System.out.println(n-cnt);
 
         br.close();
     }
