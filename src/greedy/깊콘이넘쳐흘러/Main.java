3c0e6a5e98376e6e5242ada0c67551535d1a26fc
sangki930 <sangki931@gmail.com>
2023-06-15 23:52:25 +0900

    commit 깊콘이 넘쳐흘러

--- src/greedy/깊콘이넘쳐흘러/Main.java
+++ src/greedy/깊콘이넘쳐흘러/Main.java
@@ -3,8 +3,17 @@ package greedy.깊콘이넘쳐흘러;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Gifticon{
+    long extra;
+    long limit;
+    public Gifticon(long extra, long limit){
+        this.extra=extra;
+        this.limit=limit;
+    }
+}
 
 public class Main {
 
@@ -15,7 +24,27 @@ public class Main {
         int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); // 기프티콘 남은 기한
         int[] b = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); // 며칠 뒤에 사용할 건 지
 
+        PriorityQueue<Gifticon> pq = new PriorityQueue<>(
+                (o1, o2) -> {
+                    if(o1.extra==o2.extra)
+                        return Long.compare(o1.limit,o2.limit);
+                    return Long.compare(o1.extra,o2.extra);
+                }
+        );
+        for(int i=0;i<n;i++){
+            pq.offer(new Gifticon(a[i],b[i]));
+        }
+        int answer = 0;
+        while(!pq.isEmpty()){
+            Gifticon gifticon = pq.poll();
+            if(gifticon.extra<gifticon.limit){
+                answer+=Math.ceil((gifticon.limit- gifticon.extra)/30.0);
+                gifticon.extra+=Math.ceil((gifticon.limit- gifticon.extra)/30.0)*30L;
+                pq.offer(gifticon);
+            }
+        }
 
+        System.out.println(answer);
 
         br.close();
     }
