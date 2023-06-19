d2a315fc9a165a91256f461d1d81d03e4d669cfc
sangki930 <sangki931@gmail.com>
2023-06-19 23:38:58 +0900

    commit 공주님의 정원

---
+++ src/greedy/공주님의_정원/Main.java
@@ -0,0 +1,19 @@
+package greedy.공주님의_정원;
+
+import org.w3c.dom.ranges.RangeException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        br.close();
+    }
+}
