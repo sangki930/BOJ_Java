52c8654e2d53ef81ffdc21103e8230702cbcc284
sangki930 <sangki931@gmail.com>
2023-07-14 23:56:52 +0900

    commit 스택수열

---
+++ src/stack/스택수열/Main.java
@@ -0,0 +1,22 @@
+package stack.스택수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int p = 1;
+        for(int i=0;i<n;i++){
+            int m = Integer.parseInt(br.readLine());
+            
+        }
+
+        br.close();
+    }
+}
