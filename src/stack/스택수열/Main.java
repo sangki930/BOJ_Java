a040d8d0e2a2f5452eabde05835dad1f22a07131
sangki930 <sangki931@gmail.com>
2023-07-19 23:52:21 +0900

    commit 스택수열

--- src/stack/스택수열/Main.java
+++ src/stack/스택수열/Main.java
@@ -12,10 +12,31 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
         int p = 1;
+        StringBuffer sb = new StringBuffer();
+        int[] arr = new int[n];
         for(int i=0;i<n;i++){
             int m = Integer.parseInt(br.readLine());
-            
+            arr[i] = m;
         }
+        int j=0;
+        for(int i=1;i<=n;i++){
+            if(i==arr[j]){
+                stack.push(i);
+                sb.append("+\n");
+                int k = i;
+                while(!stack.isEmpty() && stack.peek()==arr[j]){
+                    stack.pop();
+                    sb.append("-\n");j++;
+                }
+            }else{
+                stack.push(i);
+                sb.append("+\n");
+            }
+        }
+        if(!stack.isEmpty())
+            System.out.println("NO");
+        else
+            System.out.println(sb);
 
         br.close();
     }
