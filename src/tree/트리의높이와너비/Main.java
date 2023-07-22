fbeb8e4ecfa60a7c8e112cff161f50a6e5a1e328
sangki930 <sangki931@gmail.com>
2023-07-22 23:19:41 +0900

    commit 트리의 높이와 너비

---
+++ src/tree/트리의높이와너비/Main.java
@@ -0,0 +1,23 @@
+package tree.트리의높이와너비;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] graph = new ArrayList[n+1];
+
+        for(int i=1;i<=n;i++){
+            String[] tmp = br.readLine().split(" ");
+        }
+
+        br.close();
+    }
+}
