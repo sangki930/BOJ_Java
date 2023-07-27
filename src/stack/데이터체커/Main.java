450deb657530366b6a550cf6e59b51b48ccffb8d
sangki930 <sangki931@gmail.com>
2023-07-28 00:07:54 +0900

    commit 데이터체커

--- src/stack/데이터체커/Main.java
+++ src/stack/데이터체커/Main.java
@@ -11,7 +11,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
         int[][] arr = new int[n*2][];
 
         for(int i=0;i<n;i++){
