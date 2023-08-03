78897f0225ca8405017d10ad2fca74387333e61a
sangki930 <sangki931@gmail.com>
2023-08-03 23:30:06 +0900

    commit LCA

---
+++ src/tree/트리의지름/gold04/Main.java
@@ -0,0 +1,18 @@
+package tree.트리의지름.gold04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n-1;i++){
+            String[] tmp = br.readLine().split(" ");
+            int p = Integer.parseInt(tmp[0]);
+        }
+
+        br.close();
+    }
+}
