e68e94b090e1bcf6fda8df6a3af3de89697ef758
sangki930 <sangki931@gmail.com>
2023-08-22 23:46:21 +0900

    최솟값 찾기

--- src/priorityqueue/최솟값찾기/Main.java
+++ src/priorityqueue/최솟값찾기/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 
 class Data{
     int v;
