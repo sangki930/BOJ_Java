238c51a80bdc5dc969b8cc40ea6fd70127d9972e
sangki930 <sangki931@gmail.com>
2023-08-23 23:42:40 +0900

    최솟값 찾기(완)

--- src/priorityqueue/최솟값찾기/Main.java
+++ src/priorityqueue/최솟값찾기/Main.java
@@ -16,12 +16,21 @@ class Data{
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder("");
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]), l = Integer.parseInt(input[1]);
 
         input = br.readLine().split(" ");
-        LinkedList<int[]> queue = new LinkedList<>();
+        LinkedList<Data> queue = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            int v = Integer.parseInt(input[i]);
+            while(!queue.isEmpty() && queue.peekLast().v>v) queue.pollLast();
+
+            queue.offer(new Data(i,v));
+            if(queue.peek().id<i-(l-1)) queue.poll();
+            sb.append(queue.peek().v+" ");
+        }
+        System.out.println(sb);
 
         br.close();
     }
