d7fa469514433654696f60c0fa3017b6fbfee9bd
sangki930 <sangki931@gmail.com>
2023-09-07 23:38:18 +0900

    피보나치 4

--- src/dp/피보나치수4/Main.java
+++ src/dp/피보나치수4/Main.java
@@ -1,23 +1,18 @@
 package dp.피보나치수4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
 public class Main {
-
-    static long[] dp = new long[10005];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        System.out.println(f(n));
-        br.close();
-    }
-
-    public static long f(int n){
-        if(n<=1) return dp[n]=n;
-        if(dp[n]>0) return dp[n];
-        return dp[n] = f(n-1)+f(n-2);
+        BigInteger[] dp = new BigInteger[10001];
+        dp[0] = new BigInteger("0");
+        dp[1] = new BigInteger("1");
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-2].add(dp[i-1]);
+        }
+        System.out.print(dp[n]);
     }
 }
