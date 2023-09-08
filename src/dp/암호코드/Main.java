7fc5b721810c6b2443218d46409b6e29b3e5b4f3
sangki930 <sangki931@gmail.com>
2023-09-08 23:47:51 +0900

    암호코드

---
+++ src/dp/암호코드/Main.java
@@ -0,0 +1,27 @@
+package dp.암호코드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    static Map<String,Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int n = s.length();
+        int[] dp = new int[5001];
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] += dp[i-1];
+            int a = Integer.parseInt(s.substring(i-2,i));
+
+        }
+
+        br.close();
+    }
+}
