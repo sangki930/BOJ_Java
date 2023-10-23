f76f0510c44dd3507a000aedf21fdbd14be3592f
sangki930 <sangki931@gmail.com>
2023-10-23 23:41:38 +0900

    등차수열의 합 커밋

---
+++ src/math/등차수열의합/Main.java
@@ -0,0 +1,17 @@
+package math.등차수열의합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int l = Integer.parseInt(br.readLine());
+        int r = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        br.close();
+    }
+}
