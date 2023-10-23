847b3d8337debf08357c698bf5e96257327b3031
sangki930 <sangki931@gmail.com>
2023-10-24 00:31:57 +0900

    등차수열의 합 커밋

--- src/math/등차수열의합/Main.java
+++ src/math/등차수열의합/Main.java
@@ -12,6 +12,23 @@ public class Main {
         int r = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
 
+        switch (k){
+            case 2->{
+                System.out.println(Math.max(r, 2) - Math.max(2, l-1));
+            }
+            case 3->{
+                System.out.println(Math.max(r, 5)/3 - Math.max(5, l-1)/3+ "");
+            }
+            case 5->{
+                System.out.println(Math.max(r, 14)/5 - Math.max(14, l-1)/5+ "");
+            }
+            default -> {
+                int temp = Math.max(r, 9)/2 - Math.max(9, l-1)/2;
+                if (l <= 12 && 12 <= r) { temp--; }
+                System.out.println(temp + "");
+            }
+        }
+
         br.close();
     }
 }
