d0c9146017cca68554221981de8cff2ac427cb1a
sangki930 <sangki931@gmail.com>
2023-11-01 19:48:49 +0900

    암호코드

--- src/dp/암호코드/Main.java
+++ src/dp/암호코드/Main.java
@@ -3,6 +3,7 @@ package dp.암호코드;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,17 +12,32 @@ public class Main {
     static Map<String,Integer> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
-        String s = br.readLine();
-        int n = s.length();
-        int[] dp = new int[5001];
-        dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            dp[i] += dp[i-1];
-            int a = Integer.parseInt(s.substring(i-2,i));
+        int[] dp = new int[str.length()+1];
+        dp[0] = 1;
+
+        for(int i=1; i<=str.length(); i++) {
+            int now = str.charAt(i-1) - '0';
+            if(now >= 1 && now <= 9) {
+                dp[i] += dp[i-1];
+                dp[i] %= 1000000;
+            }
+
+            if(i == 1) continue; //첫번째 글자일 경우
+
+            int prev = str.charAt(i-2) - '0';
+
+            if(prev == 0) continue; //0으로 시작할경우 존재 X
+
+            int value = prev*10+now;
+
+            if(value >= 10 && value <= 26) {
+                dp[i] += dp[i-2];
+                dp[i] %= 1000000;
+            }
         }
-
-        // 11~19, 20~26
+        System.out.println(dp[str.length()]);
 
         br.close();
     }
