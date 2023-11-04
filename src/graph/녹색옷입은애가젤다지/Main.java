c708a04d5e3aa3770d9095cea82c4777998f75fd
sangki930 <sangki931@gmail.com>
2023-11-04 23:58:47 +0900

    녹색옷

--- src/graph/녹색옷입은애가젤다지/Main.java
+++ src/graph/녹색옷입은애가젤다지/Main.java
@@ -14,7 +14,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-        for(int i=1;;i++){
+        for(int i=1;;i++){ss
             int n = Integer.parseInt(br.readLine());
             if(n==0) break;
             int[][] arr = new int[n][n];
