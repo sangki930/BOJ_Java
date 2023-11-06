8cb0ec7203b7605511a3f1da692696c18c402ebb
sangki930 <sangki931@gmail.com>
2023-11-07 00:51:42 +0900

    녹색옷입은애가젤다지

--- src/graph/녹색옷입은애가젤다지/Main.java
+++ src/graph/녹색옷입은애가젤다지/Main.java
@@ -1,15 +1,29 @@
 package graph.녹색옷입은애가젤다지;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class Main {
 
-    static int[] dx = {-1,0,1,0}, dy = {0,-1,0,1};
+    static class Node{
+        int x;
+        int y;
+        int cost;
+
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+
+        }
+
+    }
+
+    static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,31 +36,35 @@ public class Main {
                 arr[j] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             }
 
-            int[][] acc = new int[n][n];
-            acc[0][0] = arr[0][0];
-
-            PriorityQueue<int[]> queue = new PriorityQueue<>(
-                    (o1,o2)->{
-                        return Integer.compare(o1[2],o2[2]);
-                    }
+            PriorityQueue<Node> queue = new PriorityQueue<>(
+                Comparator.comparingInt(o->o.cost)
             );
-            boolean[][] visited= new boolean[n][n];
-            queue.offer(new int[]{0,0,arr[0][0]});
-            visited[0][0]=true;
+            int[][] acc = new int[n][n];
+            for(int j=0;j<acc.length;j++)
+                Arrays.fill(acc[j],Integer.MAX_VALUE);
+            // boolean[][] visited= new boolean[n][n];
+            queue.offer(new Node(0,0,arr[0][0]));
+            acc[0][0] = arr[0][0];
+            int cost = 0;
             while(!queue.isEmpty()){
-                int[] p = queue.poll();
+                Node p = queue.poll();
+
+                if(p.x==n-1 && p.y==n-1) {
+                    cost = p.cost;
+                    break;
+                }
                 for(int k=0;k<4;k++){
-                    int nx = p[0]+dx[k];
-                    int ny = p[1]+dy[k];
-                    if(0<=nx && nx<n && 0<=ny && ny<n && !visited[nx][ny]){
-                        acc[nx][ny]=arr[p[0]][p[1]]+p[2];
-                        queue.offer(new int[]{nx,ny,acc[nx][ny]});
-                        visited[nx][ny] = true;
+                    int nx = p.x+dx[k];
+                    int ny = p.y+dy[k];
+                    if(0<=nx && nx<n && 0<=ny && ny<n && acc[nx][ny]>arr[nx][ny]+p.cost){
+                        acc[nx][ny]=arr[nx][ny]+p.cost;
+                        queue.offer(new Node(nx,ny,arr[nx][ny]+p.cost));
+
                     }
                 }
             }
 
-            System.out.println("Problem "+i+":"+acc[n-1][n-1]);
+            System.out.println("Problem "+i+": "+cost);
         }
 
         br.close();
