34feec0ac88aa27f0cca8998df8e035e76cfcdab
sangki930 <sangki931@gmail.com>
2023-11-17 23:18:10 +0900

    LCS2

---
+++ src/dp/LCS2/Main.java
@@ -0,0 +1,25 @@
+package dp.LCS2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n = 1001;
+    static int[][] dp = new int[n][n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
+
+        for(int i=0;i<s1.length;i++){
+            for(int j=0;j<s2.length;j++){
+
+            }
+        }
+
+        br.close();
+    }
+}
