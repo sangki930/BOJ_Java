069e9652937e0f2d9fb8b4561a04db37f00be50f
sangki930 <sangki931@gmail.com>
2023-12-05 19:18:49 +0900

    흙길보수하기(완성)

--- src/sweeping/흙길보수하기/Main.java
+++ src/sweeping/흙길보수하기/Main.java
@@ -3,6 +3,8 @@ package sweeping.흙길보수하기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 class Puddle{
     int s,e;
@@ -16,7 +18,35 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]), l =Integer.parseInt(input[1]);
+        List<Puddle> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            input = br.readLine().split(" ");
+            int s = Integer.parseInt(input[0]), e = Integer.parseInt(input[1]);
+            list.add(new Puddle(s,e));
+        }
 
+        list.sort(
+                (a,b)->{
+                    if(a.s==b.s) return Integer.compare(b.e,a.e);
+                    return Integer.compare(a.s,b.s);
+                }
+        );
+
+        int answer = 0;
+        int start = -1, end = -1;
+        for(int i=0;i<n;i++){
+            Puddle p = list.get(i);
+            if(end<=p.s){
+                start = p.s;
+                end = p.s+l*(int)Math.ceil((p.e-p.s)*1.0/l);
+                answer+=(int)Math.ceil((p.e-p.s)*1.0/l);
+            }else if(p.s<=end && end<p.e){
+                answer+=(int)Math.ceil((p.e-end)*1.0/l);
+                end += l*(int)Math.ceil((p.e-end)*1.0/l);
+            }
+            //System.out.println("start : "+start+", end : "+end+", answer : "+answer);
+        }
+        System.out.println(answer);
         br.close();
     }
 }
