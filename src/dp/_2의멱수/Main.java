0d58d717ba592b318162062c5c057f90d19c59fd
sangki930 <sangki931@gmail.com>
2023-12-15 00:54:22 +0900

    _2의 멱수

--- src/dp/_2의멱수/Main.java
+++ src/dp/_2의멱수/Main.java
@@ -7,9 +7,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+        // 1 : 1
+        // 2 : 2, 1+1
+        // 3 : 1+2, 1+1+1
+        // 4 : 4, 2+2, 1+1+2, 1+1+1+1
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<dp.length;i++){
+            if(i%2==0){
+                dp[i] = (dp[i-1]+dp[i/2])%1000000000;
+            }else{
+                dp[i] = dp[i-1];
+            }
+        }
+        System.out.println(dp[n]);
         br.close();
     }
 }
