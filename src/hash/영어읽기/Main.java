14e69a74fbf87344953e27b64b7b83d2a62a2ee1
sangki930 <sangki931@gmail.com>
2024-01-05 23:24:56 +0900

    영어읽기

--- src/hash/영어읽기/Main.java
+++ src/hash/영어읽기/Main.java
@@ -10,7 +10,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         Map<String,Integer> counter = new HashMap<>();
         for(int i=0;i<n;i++){
