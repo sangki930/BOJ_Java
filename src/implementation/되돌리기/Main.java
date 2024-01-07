2c03148bde3d5dcb3a41b5ed18c9618c3730b0e8
sangki930 <sangki931@gmail.com>
2024-01-07 23:48:15 +0900

    되돌리기

--- src/implementation/되돌리기/Main.java
+++ src/implementation/되돌리기/Main.java
@@ -3,10 +3,13 @@ package implementation.되돌리기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
 
         br.close();
     }
