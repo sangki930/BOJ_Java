99721f90bc3ff04fca12f43e5bc5910c7975b6b0
sangki930 <sangki931@gmail.com>
2024-01-14 00:37:47 +0900

    미친로봇

--- src/dfs/미친로봇/Main.java
+++ src/dfs/미친로봇/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
     static int N;
     static double e,w,s,n;
+    static boolean[][] visited = new boolean[100][100];
+    static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
+    static double[] dir;
+    static double answer = 0.0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,7 +22,28 @@ public class Main {
         w = Integer.parseInt(input[2])/100.0;
         s = Integer.parseInt(input[3])/100.0;
         n = Integer.parseInt(input[4])/100.0;
+        dir = new double[]{e,w,s,n};
 
+        move(50,50,0,1);
+
+        System.out.println(answer);
         br.close();
+    }
+
+    public static void move(int x, int y, int level, double acc){
+        if(level==N){
+            answer+=acc;
+            return ;
+        }
+        visited[x][y] = true;
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i], ny = y + dy[i];
+            if(!visited[nx][ny]){
+                visited[nx][ny] = true;
+                move(nx,ny,level+1,acc*dir[i]);
+                visited[nx][ny] = false;
+            }
+        }
+
     }
 }
