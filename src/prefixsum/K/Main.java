6f6dec26a12e8b57cdd73894018e5f4025414cf1
sangki930 <sangki931@gmail.com>
2024-01-17 20:14:20 +0900

    K(커밋)

---
+++ src/prefixsum/K/Main.java
@@ -0,0 +1,19 @@
+package prefixsum.K;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+
+        br.close();
+    }
+}
