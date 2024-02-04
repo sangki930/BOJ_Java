e089bcb4fdf8f23aece8987a047563a9aa7072b0
sangki930 <sangki931@gmail.com>
2024-02-05 00:02:00 +0900

    카드정리 01

--- src/bruteforce/카드정리01/Main.java
+++ src/bruteforce/카드정리01/Main.java
@@ -3,24 +3,35 @@ package bruteforce.카드정리01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
-        int[][] boxes = new int[n][m];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int ans = -1;
 
-        for(int i=0;i<n;i++){
-            boxes[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        boolean used[] = new boolean[m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cnt = 0;
+            int idx = -1;
+            for (int j = 0; j < m; j++) {
+                if (!st.nextToken().equals("0")) {
+                    cnt++;
+                    idx = j;
+                }
+            }
+
+            if (cnt == 1 && !used[idx]) used[idx] = true;
+            else if(cnt != 0) ans++;
         }
-        // 이동 한 번은 한 박스에서 1장이상의 카드를 빼 다른 박스에 넣는 것
 
-        int answer = Integer.MAX_VALUE;
-
-        br.close();
+        System.out.println(Math.max(ans, 0));
     }
 
 }
