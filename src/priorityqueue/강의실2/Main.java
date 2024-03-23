ec9048f70eb0dd4d83237c0d98a6322df1ceada0
sangki930 <sangki931@gmail.com>
2024-03-23 23:58:29 +0900

    강의실 2

---
+++ src/priorityqueue/강의실2/Main.java
@@ -0,0 +1,23 @@
+package priorityqueue.강의실2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        br.close();
+    }
+}
