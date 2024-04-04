986cb616ba67b450b69d8f60e3feca1a3f702a16
sangki930 <sangki931@gmail.com>
2024-04-04 20:38:14 +0900

    괄호의 값

--- src/stack/괄호의값/Main.java
+++ src/stack/괄호의값/Main.java
@@ -9,24 +9,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long answer = 0;
         String s = br.readLine();
         Stack<Character> stack = new Stack<>();
-
+        long[] arr = new long[31];
+        int j = 1;
         for(int i = 0; i < s.length(); i++) {
             if(stack.isEmpty()){
                 stack.push(s.charAt(i));
+                j++;
             }else{
-                if(stack.peek() == '('){
-
-                }else if(stack.peek() == '['){
-
+                if(stack.peek() == '(' && s.charAt(i)==')'){
+                    stack.pop();
+                    if(arr[j]==0) arr[j]=1L;
+                    arr[j-1] += arr[j]*2;
+                    arr[j--] = 0;
+                }else if(stack.peek() == '[' && s.charAt(i)==']'){
+                    stack.pop();
+                    if(arr[j]==0) arr[j]=1L;
+                    arr[j-1] += arr[j]*3;
+                    arr[j--] = 0;
                 }else{
-
+                    stack.push(s.charAt(i));
+                    j++;
                 }
             }
         }
 
+        if(!stack.isEmpty()){
+            System.out.println(0);
+            br.close();
+            return ;
+        }
+        System.out.println(arr[1]);
         br.close();
     }
 }
