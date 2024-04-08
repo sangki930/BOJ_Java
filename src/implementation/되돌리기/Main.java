98aea643be973bb8dca12c20cd3b72c41913ea49
sangki930 <sangki931@gmail.com>
2024-04-09 00:09:05 +0900

    되돌리기

--- src/implementation/되돌리기/Main.java
+++ src/implementation/되돌리기/Main.java
@@ -3,15 +3,17 @@ package implementation.되돌리기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
-class Pair{
-    String text;
+class Log{
+    String s;
     int time;
-    public Pair(String text, int time){
-        this.text=text;
-        this.time=time;
+    public Log(String s, int time){
+        this.s = s;
+        this.time = time;
     }
+
 }
 
 public class Main {
@@ -19,31 +21,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        Stack<Pair> stack = new Stack<>();
-
-        for(int i=0;i<n;i++){
+        List<Log> logs = new ArrayList<Log>();
+        logs.add(new Log("", 0));
+        for(int i=1;i<=n;i++){
             String[] input = br.readLine().split(" ");
-            String cmd = input[0];
-            switch (cmd){
+            int time = Integer.parseInt(input[2]);
+            switch (input[0]){
                 case "type"->{
-                    String text = input[1];
-                    int time = Integer.parseInt(input[2]);
-                    stack.push(new Pair(text,time));
+                    logs.add(new Log(logs.get(i-1).s+input[1],time));
                 }
                 case "undo"->{
-                    int range = Integer.parseInt(input[1]);
-                    int time = Integer.parseInt(input[2]);
-                    while(!stack.isEmpty() && (time-range)<=stack.peek().time){
-                        stack.pop();
+                    boolean flag = false;
+                    int start = time - Integer.parseInt(input[1])-1;
+                    for(int j=i-1;j>=0;j--){
+                        if(start<0) break;
+                        Log log = logs.get(j);
+
+                        if(log.time<=start){
+                            logs.add(new Log(log.s,time));
+                            flag = true;
+                            break;
+                        }
                     }
+
+                    if(!flag) logs.add(new Log("",time));
                 }
             }
         }
-        String answer = "";
-        while(!stack.isEmpty()){
-            answer  = stack.pop().text+answer;
-        }
-        System.out.println(answer);
+        System.out.println(logs.get(n).s);
         br.close();
     }
 }
