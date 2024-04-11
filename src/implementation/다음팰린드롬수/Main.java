c603c276ec7fe985f7c381d6561f3f8fd7840703
sangki930 <sangki931@gmail.com>
2024-04-11 23:52:13 +0900

    다음팰린드롬수

---
+++ src/implementation/다음팰린드롬수/Main.java
@@ -0,0 +1,15 @@
+package implementation.다음팰린드롬수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        br.close();
+    }
+}
