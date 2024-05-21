197575ffb6b121744c23020a7f4c95ab1596d82b
sangki930 <sangki931@gmail.com>
2024-05-21 23:52:29 +0900

    멍멍이 쓰다듬기

--- src/math/멍멍이쓰다듬기/Main.java
+++ src/math/멍멍이쓰다듬기/Main.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] input = br.readLine().split(" ");
         long x = Integer.parseInt(input[0]), y = Integer.parseInt(input[1]);
         long diff = y-x;
