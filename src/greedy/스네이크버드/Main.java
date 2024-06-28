a828e76a4e677722fb71708c7947c3eae0c099be
sangki930 <sangki931@gmail.com>
2024-06-28 18:55:01 +0900

    스네이크버드

---
+++ src/greedy/스네이크버드/Main.java
@@ -0,0 +1,25 @@
+package greedy.스네이크버드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]), h = Integer.parseInt(input[1]);
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        int answer = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>h) break;
+            h++;answer++;
+        }
+        System.out.println(h);
+        br.close();
+    }
+}
