bbc449c1f2c1e699ccece33457054cfe6d952685
sangki930 <sangki931@gmail.com>
2025-03-09 21:00:19 +0900

    사람의 수

---
+++ src/math/사람의수/Main.java
@@ -0,0 +1,44 @@
+package math.사람의수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = IntStream.range(0, n)
+                .map(i -> {
+                    try {
+                        return Integer.parseInt((br.readLine().split("\\.")[1]));
+                    } catch (IOException e) {
+                        return 0;
+                    }
+                })
+                .toArray();
+        int i = 1;
+        loop : for(;;){
+            boolean flag = true;
+            for(int j=0;j<arr.length;j++){
+
+                int tmp = i*arr[j];
+                if(tmp%1000 !=0) tmp = tmp - (tmp%1000)+1000;
+                int num = tmp/i;
+                if(num != arr[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println(i);
+                break loop;
+            }
+            i++;
+        }
+
+        br.close();
+    }
+}
