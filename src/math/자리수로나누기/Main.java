168139368d3aaa478d2a783a1ea0b9e974d6c02f
sangki930 <sangki931@gmail.com>
2025-03-20 23:57:15 +0900

    자리수로 나누기

---
+++ src/math/자리수로나누기/Main.java
@@ -0,0 +1,15 @@
+package math.자리수로나누기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        br.close();
+    }
+}
